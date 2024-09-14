84d1fecf37a998c30e8772c74474894f8b5b4aca
Leon Kenyaga <wolfofwallstreet@kali.Leon.com>
2024-09-14 15:27:50 +0300

    feat(footer): create initial footer component structure

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="w-full h-[224px] border-t-2 border-red absolute bottom-0 mb-[36px] flex flex-wrap justify-center gap-[13px] md:gap-[92px] lg:gap-[130px] pt-[21px] md:pt-[40px]"></footer>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import * as React from "react"
+import Footer from "../components/footer"
+
+export default function Home() {
+  return (
+  <div className="relative min-h-screen">
+  <Footer />
+</div>
+  )
+}
