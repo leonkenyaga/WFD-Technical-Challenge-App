d7fd30ea5721aa414d9ef40e41aabc37f006f5e6
Leon Kenyaga <wolfofwallstreet@kali.Leon.com>
2024-09-15 10:59:21 +0300

    feat(footer): create about-company subcomponent

---
+++ src/components/footer/about-company/about.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+
+export default function About() {
+  return (
+    <nav className="pt-[11px] font-light text-ms text-darkgrey" style={{ fontFamily: "Roboto" }}>
+      <ul className="max-sm:flex max-sm:flex-wrap gap-4 justify-center">
+        <li className="hover:text-red">
+          <a href="#">About</a>
+        </li>
+        <li className="hover:text-red">
+          <a href="#">Contact</a>
+        </li>
+        <li className="hover:text-red">
+          <a href="#">Press</a>
+        </li>
+        <li className="hover:text-red">
+          <a href="#">Blog</a>
+        </li>
+        <li className="hover:text-red">
+          <a href="#">Terms and Privacy</a>
+        </li>
+        <li className="hover:text-red">
+          <a href="#">Help</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,10 +1,12 @@
 import * as React from "react";
 import Dreamshare from "./dreamshare";
+import About_Company from "./about-company";
 
 export default function Footer() {
   return (
     <footer className="w-full h-[224px] border-t-2 border-red absolute bottom-0 mb-[36px] flex flex-wrap justify-center gap-[13px] md:gap-[92px] lg:gap-[130px] pt-[21px] md:pt-[40px]">
       <Dreamshare />
+      <About_Company />
     </footer>
   );
 }
