44479271461de61d29a9563f00f08d837da847e2
Leon Kenyaga <wolfofwallstreet@kali.Leon.com>
2024-09-16 09:04:28 +0300

    feat(footer): create dreamshare subcomponent

---
+++ src/components/footer/dreamshare.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+export default function Dreamshare() {
+  return (
+    <section className="w-[280px] md:w-[114px]">
+      <h1
+        style={{ fontFamily: "Montserrat Variable" }}
+        className="text-red text-base font-normal max-sm:flex justify-center"
+      >
+        DREAMSHARE
+      </h1>
+    </section>
+  );
+}
--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,7 +1,10 @@
 import * as React from "react";
+import Dreamshare from "./dreamshare";
 
 export default function Footer() {
   return (
-    <footer className="w-full h-[224px] border-t-2 border-red absolute bottom-0 mb-[36px] flex flex-wrap justify-center gap-[13px] md:gap-[92px] lg:gap-[130px] pt-[21px] md:pt-[40px]"></footer>
+    <footer className="w-full h-[224px] border-t-2 border-red absolute bottom-0 mb-[36px] flex flex-wrap justify-center gap-[13px] md:gap-[92px] lg:gap-[130px] pt-[21px] md:pt-[40px]">
+      <Dreamshare />
+    </footer>
   );
 }
