1a4fc8eacbb8ac31607f2c1a3d383ea43e18086e
Leon Kenyaga <wolfofwallstreet@kali.Leon.com>
2024-09-17 18:53:53 +0300

    fix(footer): Correct footer styling to match header

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -6,7 +6,7 @@ import Designedby from "./Designedby";
 
 export default function Footer() {
   return (
-    <footer className="w-full h-[224px] border-t-2 border-red absolute bottom-0 mb-[36px] flex flex-wrap justify-center gap-[13px] md:gap-[92px] lg:gap-[130px] pt-[21px] md:pt-[40px]">
+    <footer className="w-full h-[224px] border-t-2 border-red mb-[36px] flex flex-wrap justify-center gap-[13px] md:gap-[92px] lg:gap-[130px] pt-[21px] md:pt-[40px]">
       <Dreamshare />
       <About_Company />
       <Partners />
