b976bca07f4e9ee415446e361e9f38129692cd72
Leon Kenyaga <wolfofwallstreet@kali.Leon.com>
2024-09-19 09:42:18 +0300

    feat(main): create main component layout

---
+++ src/components/main/index.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+
+export default function Main() {
+  return <main className="grid justify-items-center"></main>;
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,14 @@
 import * as React from "react";
 import Footer from "../components/footer";
 import Header from "../components/header";
+import Main from "../components/main";
 
-export default function Home(props) {
+export default function Home() {
+
   return (
     <div className="relative min-h-screen">
       <Header />
+      <Main />
       <Footer />
     </div>
   );
