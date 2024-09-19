27deadc300f2d7e1a9740e90b4d343486c7384b9
Leon Kenyaga <wolfofwallstreet@kali.Leon.com>
2024-09-19 16:49:41 +0300

    build(config): configure gatsby-source-contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,5 +22,13 @@ module.exports = {
         path: `${__dirname}/src/images/`,
       },
     },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
   ],
 }
