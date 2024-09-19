ec0daae2c978ec1e443de30995588abfc5ef2887
Leon Kenyaga <wolfofwallstreet@kali.Leon.com>
2024-09-19 11:51:16 +0300

    feat(main): create most-popular-movies component layout

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,5 +1,10 @@
 import * as React from "react";
+import MostPopularMovies from "./most_popular_movies";
 
 export default function Main() {
-  return <main className="grid justify-items-center"></main>;
+  return (
+    <main className="grid justify-items-center">
+      <MostPopularMovies />
+    </main>
+  );
 }
---
+++ src/components/main/most_popular_movies/index.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+export default function MostPopularMovies() {
+  return (
+    <section className="w-[300px] md:w-[748px] lg:w-[940px] mt-[95px] mb-[36px] flex flex-wrap justify-start gap-[20px] md:gap-[20px] lg:gap-[20px]"></section>
+  );
+}
