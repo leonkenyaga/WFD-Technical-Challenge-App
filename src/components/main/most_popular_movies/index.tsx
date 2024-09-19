efffc3917852742e08cffd1366954c7b36e1ff2d
Leon Kenyaga <wolfofwallstreet@kali.Leon.com>
2024-09-19 21:34:17 +0300

    feat(most-popular-movies): create movies-section-title subcomponent

--- src/components/main/most_popular_movies/index.tsx
+++ src/components/main/most_popular_movies/index.tsx
@@ -1,7 +1,10 @@
 import * as React from "react";
+import MoviesSectionTitle from "./movies_section_title";
 
 export default function MostPopularMovies() {
   return (
-    <section className="w-[300px] md:w-[748px] lg:w-[940px] mt-[95px] mb-[36px] flex flex-wrap justify-start gap-[20px] md:gap-[20px] lg:gap-[20px]"></section>
+    <section className="w-[300px] md:w-[748px] lg:w-[940px] mt-[95px] mb-[36px] flex flex-wrap justify-start gap-[20px] md:gap-[20px] lg:gap-[20px]">
+      <MoviesSectionTitle />
+    </section>
   );
 }
