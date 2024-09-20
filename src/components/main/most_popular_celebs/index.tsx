f5022aca0d0c8724a6a0bb8de61bcdfc0a802530
Leon Kenyaga <wolfofwallstreet@kali.Leon.com>
2024-09-20 12:52:58 +0300

    feat(most-popular-celebs): add section title subcomponent

--- src/components/main/most_popular_celebs/index.tsx
+++ src/components/main/most_popular_celebs/index.tsx
@@ -1,7 +1,10 @@
 import * as React from "react";
+import SectionTitle from "../../shared_components/sectiontitle";
 
 export default function MostPopularCelebs() {
   return (
-    <section className="w-[300px] md:w-[470px] lg:w-[941px] mt-[95px] mb-[36px] flex flex-wrap md:justify-start gap-[20px] md:gap-[20px] lg:gap-[18px] justify-center"></section>
+    <section className="w-[300px] md:w-[470px] lg:w-[941px] mt-[95px] mb-[36px] flex flex-wrap md:justify-start gap-[20px] md:gap-[20px] lg:gap-[18px] justify-center">
+      <SectionTitle>Most Popular Celebs</SectionTitle>
+    </section>
   );
 }
