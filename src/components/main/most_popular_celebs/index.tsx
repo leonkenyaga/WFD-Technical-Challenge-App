5ee924710d2498d062edf5703eb128ecc0ff679a
Leon Kenyaga <wolfofwallstreet@kali.Leon.com>
2024-09-20 12:36:58 +0300

    feat(most-popular-celebs): create most-popular-celebs component layout

---
+++ src/components/main/most_popular_celebs/index.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+export default function MostPopularCelebs() {
+  return (
+    <section className="w-[300px] md:w-[470px] lg:w-[941px] mt-[95px] mb-[36px] flex flex-wrap md:justify-start gap-[20px] md:gap-[20px] lg:gap-[18px] justify-center"></section>
+  );
+}
