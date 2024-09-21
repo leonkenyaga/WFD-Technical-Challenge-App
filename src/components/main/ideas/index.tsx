a0f8d1c476889af50785154aea9c6e0716f69833
Leon Kenyaga <wolfofwallstreet@kali.Leon.com>
2024-09-21 12:28:33 +0300

    feat(ideas): add ideas section title

--- src/components/main/ideas/index.tsx
+++ src/components/main/ideas/index.tsx
@@ -1,7 +1,10 @@
 import * as React from "react";
+import SectionTitle from "../../shared_components/sectiontitle";
 
 export default function Ideas() {
   return (
-    <section className="w-[300px] md:w-[748px] lg:w-[940px] mt-[95px] mb-[36px] flex flex-wrap md:justify-between justify-center gap-[20px]"></section>
+    <section className="w-[300px] md:w-[748px] lg:w-[940px] mt-[95px] mb-[36px] flex flex-wrap md:justify-between justify-center gap-[20px]">
+      <SectionTitle>Discover holiday activity ideas</SectionTitle>
+    </section>
   );
 }
