8fdabd25f49009dc28c3b9237dd285a352deddee
Leon Kenyaga <wolfofwallstreet@kali.Leon.com>
2024-09-21 12:34:45 +0300

    feat(ideas): create ideas component layout

---
+++ src/components/main/ideas/index.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+export default function Ideas() {
+  return (
+    <section className="w-[300px] md:w-[748px] lg:w-[940px] mt-[95px] mb-[36px] flex flex-wrap md:justify-between justify-center gap-[20px]"></section>
+  );
+}
--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,12 +1,14 @@
 import * as React from "react";
 import MostPopularMovies from "./most_popular_movies";
 import MostPopularCelebs from "./most_popular_celebs";
+import Ideas from "./ideas";
 
 export default function Main() {
   return (
     <main className="grid justify-items-center">
       <MostPopularMovies />
       <MostPopularCelebs />
+      <Ideas />
     </main>
   );
 }
